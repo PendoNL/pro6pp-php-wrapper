0f8d6571796e27cc2640d242a08d173ba0593740
Joshua de Gier <info@pendo.nl>
2016-11-11 03:31:33 +0100

    First Version

---
+++ src/Pro6pp.php
@@ -0,0 +1,225 @@
+<?php
+
+namespace PendoNL\Pro6pp;
+
+class Pro6pp
+{
+
+    private $api_format = '';
+
+    private $api_pretty = false;
+
+    private $api_key = '';
+
+    private $api_host = 'http://api.pro6pp.nl/v1/';
+
+    private $data = [];
+
+    /**
+     * Pro6pp constructor.
+     *
+     * @param $api_key
+     * @param $format
+     * @param boolean $pretty
+     */
+    public function __construct($api_key, $format = 'json', $pretty = false) {
+        $this->api_key = $api_key;
+        $this->api_format = $format;
+        $this->api_pretty = $pretty;
+    }
+
+    /**
+     * @return string
+     */
+    public function getApikey() {
+        return $this->api_key;
+    }
+
+    /**
+     * Get a full address after providing a postalcode and number, the results also
+     * include the coordinates from the address.
+     *
+     * @param $postalcode
+     * @param $number
+     *
+     * @return mixed
+     */
+    public function autocomplete($postalcode, $number = '', $extension = '') {
+        $postalcode = $this->determinePostalType($postalcode);
+
+        $this->data = array_merge($postalcode, ['streetnumber' => $number, 'extension' => $extension]);
+
+        return $this->call('autocomplete', $this->prepareData($this->data));
+    }
+
+    /**
+     * Find an address by their coordinates, returns a full address if found.
+     *
+     * @param $lat
+     * @param $lng
+     *
+     * @return mixed
+     */
+    public function reverse($lat, $lng) {
+        $this->data = ['lat' => $lat, 'lng' => $lng];
+
+        return $this->call('reverse', $this->prepareData($this->data));
+    }
+
+    /**
+     * Returns a list of postalcodes sorted from nearest to farest. Either include a lat/lng
+     * combination or a nl_fourpp in the optional array.
+     *
+     * @param $target_nl_fourpps
+     * @param array $optional
+     *
+     * @return mixed
+     */
+    public function locator($target_nl_fourpps, $optional = []) {
+        $this->data['target_nl_fourpps'] = implode(",", $target_nl_fourpps);
+
+        $this->data = array_merge($this->data, $optional);
+
+        return $this->call('locator', $this->prepareData($this->data));
+    }
+
+    /**
+     * Returns a list of postalcodes within a given range.
+     *
+     * @param $nl_fourpp
+     * @param int $range
+     * @param int $per_page
+     * @param int $page
+     *
+     * @return mixed
+     */
+    public function range($nl_fourpp, $range = 5000, $per_page = 10, $page = 1) {
+        $this->data = ['nl_fourpp' => $nl_fourpp, 'range' => $range, 'per_page' => $per_page, 'page' => $page];
+
+        return $this->call('range', $this->prepareData($this->data));
+    }
+
+    /**
+     * Autocompletes a city name
+     *
+     * @param $nl_city
+     * @param int $per_page
+     *
+     * @return mixed
+     */
+    public function suggest($nl_city, $per_page = 10) {
+        $this->data = ['nl_city' => $nl_city, 'per_page' => $per_page];
+
+        return $this->call('suggest', $this->prepareData($this->data));
+    }
+
+    /**
+     * Calculates the distance between two nl_fourpp postalcodes. Optional you can choose between
+     * road or straight distance.
+     *
+     * @param $from_nl_fourpp
+     * @param $to_nl_fourpp
+     * @param $algorithm
+     *
+     * @return mixed
+     */
+    public function distance($from_nl_fourpp, $to_nl_fourpp, $algorithm = 'road') {
+        $this->data = ['from_nl_fourpp' => $from_nl_fourpp, 'to_nl_fourpp' => $to_nl_fourpp, 'algorithm' => $algorithm];
+
+        return $this->call('distance', $this->prepareData($this->data));
+    }
+
+    /**
+     * Helper method to calculate the distance between two coordinates.
+     *
+     * @param $lat1
+     * @param $lng1
+     * @param $lat2
+     * @param $lng2
+     *
+     * @param bool $miles
+     *
+     * @return float
+     */
+    public function coordinate_distance($lat1, $lng1, $lat2, $lng2, $miles = false)
+    {
+        $pi80 = M_PI / 180;
+        $lat1 *= $pi80;
+        $lng1 *= $pi80;
+        $lat2 *= $pi80;
+        $lng2 *= $pi80;
+
+        $r = 6372.797;
+        $dlat = $lat2 - $lat1;
+        $dlng = $lng2 - $lng1;
+
+        $a = sin($dlat / 2) * sin($dlat / 2) + cos($lat1) * cos($lat2) * sin($dlng / 2) * sin($dlng / 2);
+        $c = 2 * atan2(sqrt($a), sqrt(1 - $a));
+        $km = $r * $c;
+
+        return ($miles ? ($km * 0.621371192) : $km);
+    }
+
+    /**
+     * Checks if one of two valid postal types was sent along.
+     *
+     * @param $postalcode
+     *
+     * @return array
+     *
+     * @throws \Exception
+     */
+    public function determinePostalType($postalcode) {
+        $postalcode = str_replace(" ", "", $postalcode);
+
+        if(strlen($postalcode) == 6) {
+            return ['nl_sixpp' => $postalcode];
+        }
+
+        if(strlen($postalcode) == 4) {
+            return ['nl_fourpp' => $postalcode];
+        }
+
+        throw new \Exception('No valid postalcode was found (nl_sixpp or nl_fourpp)');
+    }
+
+    /**
+     * Make sure the data contains the api_key and desired format.
+     *
+     * @param $data
+     *
+     * @return mixed
+     */
+    protected function prepareData($data) {
+        $data['auth_key'] = $this->api_key;
+        $data['format'] = $this->api_format;
+        $data['pretty'] = $this->api_pretty;
+
+        return $data;
+    }
+
+    /**
+     * Method to do the actual call to Pro6PP's API and return the data
+     * in the given format.
+     *
+     * @param $path
+     * @param $data
+     *
+     * @return mixed
+     */
+    protected function call($path, $data) {
+        $url = $this->api_host . $path .'?'. http_build_query($data);
+
+        $ch = curl_init($url);
+
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_TIMEOUT, 5);
+
+        $return = curl_exec($ch);
+
+        curl_close($ch);
+
+        return $return;
+    }
+
+}
---
+++ tests/Pro6ppTest.php
@@ -0,0 +1,129 @@
+<?php
+
+use PendoNL\Pro6pp\Pro6pp;
+
+class Pro6ppTest extends \PHPUnit\Framework\TestCase
+{
+
+    /**
+     * Test if the API key gets set when constructing
+     */
+    public function test_it_sets_api_key() {
+        $pro6pp = new Pro6pp('api_key');
+
+        $this->assertEquals('api_key', $pro6pp->getApiKey());
+    }
+
+    /**
+     * Test if the autocomplete method works
+     */
+    public function test_it_autocompletes() {
+        $data = '{"status":"ok","results":[{"extension":"C","nl_sixpp":"6225XS","street":"Geusseltweg","city":"Maastricht","municipality":"Maastricht","province":"Limburg","lat":50.8579834,"lng":5.7174217,"areacode":"043","streetnumber":7}]}';
+
+        $stub = $this->createMock(Pro6pp::class);
+
+        $stub->method('autocomplete')
+            ->willReturn($data);
+
+        $this->assertEquals($data, $stub->autocomplete('6225XS', 7, 'c'));
+    }
+
+    /**
+     * Test if the reverse method works
+     */
+    public function test_it_reverses() {
+        $data = '{"status":"ok","results":{"precision":8,"province":"Limburg","municipality":"Maastricht","city":"Maastricht","nl_fourpp":6225,"lat":50.857970000000002,"lng":5.7173999999999996,"nl_sixpp":"6225XS","streets":["Geusseltweg"]}}';
+
+        $stub = $this->createMock(Pro6pp::class);
+
+        $stub->method('reverse')
+            ->willReturn($data);
+
+        $this->assertEquals($data, $stub->reverse(50.858030, 5.717376));
+    }
+
+    /**
+     * Test if the locator method works
+     */
+    public function test_it_locates() {
+        $data = '{"status":"ok","results":[{"nl_fourpp":6225,"distance":0,"lat":50.86277,"lng":5.73217,"city":"Maastricht","municipality":"Maastricht","province":"Limburg"},{"nl_fourpp":6223,"distance":3606,"lat":50.88435,"lng":5.69387,"city":"Maastricht","municipality":"Maastricht","province":"Limburg"}]}';
+
+        $stub = $this->createMock(Pro6pp::class);
+
+        $stub->method('locator')
+            ->willReturn($data);
+
+        $this->assertEquals($data, $stub->locator([6223, 6225], ['nl_fourpp' => 6225]));
+    }
+
+    /**
+     * Test if the range method works
+     */
+    public function test_it_finds_within_range() {
+        $data = '{"status":"ok","results":[{"nl_fourpp":"6225","distance":0,"lat":50.8628,"lng":5.7322},{"nl_fourpp":"6222","distance":1445,"lat":50.8659,"lng":5.7122}]}';
+
+        $stub = $this->createMock(Pro6pp::class);
+
+        $stub->method('range')
+            ->willReturn($data);
+
+        $this->assertEquals($data, $stub->range(6225, 1500));
+    }
+
+    /**
+     * Test if the distance method works
+     */
+    public function test_it_calculates_distance() {
+        $data = '{"status":"ok","results":{"distance":4522}}';
+
+        $stub = $this->createMock(Pro6pp::class);
+
+        $stub->method('distance')
+            ->willReturn($data);
+
+        $this->assertEquals($data, $stub->distance(6225, 6214));
+    }
+
+    /**
+     * Test if the suggest method works
+     */
+    public function test_it_suggests() {
+        $data = '{"status":"ok","results":[{"city_key":"agxlfnBybzZwcC1hcGlyHAsSDENpdHlTdWdnZXN0MiIKbWFhc2JvbW1lbAw","city":"Maasbommel","official_city":"Maasbommel","nl_fourpps":"6627","province":"Gelderland","lat":51.82719,"lng":5.53366}]}';
+
+        // Create a stub for the Pro6pp class.
+        $stub = $this->createMock(Pro6pp::class);
+
+        // Configure the stub.
+        $stub->method('suggest')
+            ->willReturn($data);
+
+        $this->assertEquals($data, $stub->suggest('maas', 1));
+    }
+
+    /**
+     * Test if the distance between coordiantes method works
+     */
+    public function test_it_calculates_distance_by_coordinates() {
+        $Pro6pp = new Pro6pp('api_key');
+
+        $this->assertEquals(4.5435663553281715, $Pro6pp->coordinate_distance(50.858030, 5.717376, 50.840078, 5.659258));
+    }
+
+    /**
+     * Test to see if the code can determine a nl_fourpp postal
+     */
+    public function test_it_can_determine_fourpp_postal() {
+        $Pro6pp = new Pro6pp('api_key');
+
+        $this->assertEquals('6225', $Pro6pp->determinePostalType('6225')['nl_fourpp']);
+    }
+
+    /**
+     * Test to see if the code can determine a nl_fourpp postal
+     */
+    public function test_it_can_determine_sixpp_postal() {
+        $Pro6pp = new Pro6pp('api_key');
+
+        $this->assertEquals('6225XS', $Pro6pp->determinePostalType('6225XS')['nl_sixpp']);
+    }
+}
